157ec07a9962ba9d2a4a48ca05ffeef8f906bb91
CptnTeemo <69323608+CptnTeemo@users.noreply.github.com>
2022-11-07 22:19:07 +0300

    Homework 3 was added
    Homework 3 implementation

---
+++ homework_3_BookShop/MyBookShopApp_main/src/main/java/com/example/MyBookShopApp/data/Author.java
@@ -0,0 +1,31 @@
+package com.example.MyBookShopApp.data;
+
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
